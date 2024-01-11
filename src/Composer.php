3721e3b2833f1b679fa8de0ce404d81acab9d906
exeweb <exeweb@163.com>
2024-01-11 17:16:57 +0800

    commit_message

---
+++ src/Composer.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Exewen\Utils;
+
+use Illuminate\Support\Collection;
+
+class Composer
+{
+
+    /**
+     * @var array
+     */
+    private static array $extra = [];
+
+    /**
+     * 获取composer.lock extra合并信息
+     * @param string|null $key
+     * @return array
+     */
+    public static function getPackageExtra(string $key = null): array
+    {
+        if (!self::$extra) {
+            self::getExtraContent();
+//            self::getSelfExtraContent();
+        }
+        if ($key === null) {
+            return self::$extra;
+        }
+
+        $extra = [];
+        foreach (self::$extra ?? [] as $config) {
+            foreach ($config ?? [] as $configKey => $item) {
+                if ($key === $configKey && $item) {
+                    foreach ($item as $k => $v) {
+                        if (is_array($v)) {
+                            $extra[$k] = array_merge($extra[$k] ?? [], $v);
+                        } else {
+                            $extra[$k][] = $v;
+                        }
+                    }
+                }
+            }
+        }
+        return $extra;
+    }
+
+    /**
+     * 获取composer.lock extra信息
+     * @return void
+     */
+    public static function getExtraContent()
+    {
+        $path = self::composerLockFile();
+        if (!$path) {
+            throw new \RuntimeException('composer.lock not found.');
+        }
+        $content = new Collection(json_decode(file_get_contents($path), true));
+        $packagesDev = $content->offsetGet('packages-dev') ?? [];
+        $packages = $content->offsetGet('packages') ?? [];
+        foreach (array_merge($packages, $packagesDev) as $package) {
+            $packageName = '';
+            foreach ($package ?? [] as $key => $value) {
+                if ($key === 'name') {
+                    $packageName = $value;
+                    continue;
+                }
+
+                if ($key == 'extra') {
+                    $packageName && self::$extra[$packageName] = $value;
+                }
+            }
+        }
+    }
+
+    public static function getSelfExtraContent()
+    {
+        $path = self::composerFile();
+        if (!$path) {
+            throw new \RuntimeException('composer.json not found.');
+        }
+        $selfComposer = json_decode(file_get_contents($path), true);
+        $packageName = $selfComposer['name'] ?? '';
+        $extraValue = $selfComposer['extra'] ?? [];
+        self::$extra[$packageName] = $extraValue;
+    }
+
+    /**
+     * 查找composer.lock路径
+     * @return string
+     */
+    public static function composerLockFile(): string
+    {
+        $path = '';
+        if (is_readable(BASE_PATH_PKG . '/composer.lock')) {
+            $path = BASE_PATH_PKG . '/composer.lock';
+        }
+        return $path;
+    }
+
+    public static function composerFile(): string
+    {
+        $path = '';
+        if (is_readable(BASE_PATH_PKG . '/composer.json')) {
+            $path = BASE_PATH_PKG . '/composer.json';
+        }
+        return $path;
+    }
+
+}
---
+++ src/FileUtil.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace Exewen\Utils;
+
+class FileUtil
+{
+    private static string $snapshotPath = BASE_PATH_PKG . "/config/nacos/env";
+
+    /**
+     * 设置配置储存地址
+     * @param string $snapshotPath
+     * @return string
+     */
+    public static function setSnapshotPath(string $snapshotPath): string
+    {
+        return self::$snapshotPath = $snapshotPath;
+    }
+
+    /**
+     * 保存配置文件
+     * @param string $namespaceId
+     * @param string $dataId
+     * @param string $group
+     * @param $dataIdConfig
+     * @return void
+     */
+    public static function saveSnapshot(string $namespaceId, string $dataId, string $group, $dataIdConfig)
+    {
+        $snapshotFile = self::getSnapshotFile($namespaceId, $dataId, $group);
+        @unlink($snapshotFile);
+        if (!$dataIdConfig) {
+            @unlink($snapshotFile);
+        } else {
+            $file = new \SplFileInfo($snapshotFile);
+            if (!is_dir($file->getPath())) {
+                mkdir($file->getPath(), 0777, true);
+            }
+            file_put_contents($snapshotFile, $dataIdConfig);
+        }
+    }
+
+    /**
+     * 获取配置文件地址
+     * @param string $namespaceId
+     * @param string $dataId
+     * @param string $group
+     * @return string
+     */
+    public static function getSnapshotFile(string $namespaceId, string $dataId, string $group = 'DEFAULT_GROUP'): string
+    {
+        return self::getSnapshotPath() . DIRECTORY_SEPARATOR . $namespaceId . DIRECTORY_SEPARATOR . $group . DIRECTORY_SEPARATOR . $dataId;
+    }
+
+    private static function getSnapshotPath(): string
+    {
+        return self::$snapshotPath;
+    }
+
+
+}

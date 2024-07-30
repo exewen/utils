31797d364fa22eda6491966d546787ebad7f14ed
exewen <exeweb@163.com>
2024-07-30 14:56:45 +0800

    perf:更新ContainerInterface兼容

--- src/Contract/ContainerInterface.php
+++ src/Contract/ContainerInterface.php
@@ -10,33 +10,26 @@ namespace Exewen\Utils\Contract;
 interface ContainerInterface
 {
     /**
-     * 获取实例
-     * @param string $id
-     * @return mixed
+     * Finds an entry of the container by its identifier and returns it.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     *
+     * @return mixed Entry.
      */
     public function get(string $id);
 
     /**
-     * 是否存在实例
-     * @param string $id
-     * @return mixed
+     * Returns true if the container can return an entry for the given identifier.
+     * Returns false otherwise.
+     *
+     * `has($id)` returning true does not mean that `get($id)` will not throw an exception.
+     * It does however mean that `get($id)` will not throw a `NotFoundExceptionInterface`.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @return bool
      */
     public function has(string $id): bool;
-
-    /**
-     * 绑定实例或者闭包
-     * @param string $abstract
-     * @param $concrete
-     * @return mixed
-     */
-    public function bind(string $abstract, $concrete = null);
-
-    /**
-     * 绑定单例
-     * @param string $abstract
-     * @param $concrete
-     * @return mixed
-     */
-    public function singleton(string $abstract, $concrete = null);
 
 }
